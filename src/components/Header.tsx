418eede089fceea923cab8cd5f3753d4535c1f0f
Maxat <maksat.bek93@gmail.com>
2024-05-02 11:34:56 +0500

    add input search ui

---
+++ src/components/Header.tsx
@@ -0,0 +1,43 @@
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerMenuContainer}>
+        <Image
+          style={styles.headerMenu}
+          source={require('../assets/HeaderMenu.png')}
+        />
+      </View>
+      <Image style={styles.dp} source={require('../assets/AccountW.png')} />
+    </View>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerMenu: {
+    height: 28,
+    width: 28,
+  },
+  headerMenuContainer: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 22,
+    height: 44,
+    width: 44,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dp: {
+    height: 44,
+    width: 44,
+    borderRadius: 22,
+  },
+});
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,36 @@
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import Header from '../components/Header';
+import Fontisto from 'react-native-vector-icons/dist/Fontisto';
 
 export default function HomeScreen() {
   return (
     <LinearGradient colors={['#FDF0F3', '#FFFBFC']} style={styles.container}>
-      <Text>Home!</Text>
+      <Header />
+      <Text style={styles.matchText}>Match Your Style</Text>
+      <View style={styles.searchContainer}>
+        <View style={styles.iconContainer}>
+          <Fontisto name={'search'} size={26} color={'#C0C0C0'} />
+        </View>
+        <TextInput style={styles.input} placeholder="Search" />
+      </View>
     </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  container: {flex: 1, padding: 20},
+  matchText: {fontSize: 28, color: 'black', marginTop: 24, lineHeight: 42},
+  searchContainer: {
+    backgroundColor: 'white',
+    height: 48,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 20,
+    borderRadius: 12,
+  },
+  iconContainer: {
+    marginHorizontal: 15,
+  },
+  input: {flex: 1},
 });
